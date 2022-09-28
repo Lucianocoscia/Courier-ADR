fe4911fd61aa732dd6a3f66834461be709482283
Luciano Coscia <lucianocoscia5@gmail.com>
2022-09-28 18:46:46 -0300

    Modificando cotizador y animaciones

---
+++ js/categorias.js
@@ -0,0 +1,165 @@
+const categorias = [
+
+    {
+        nombre: "Muebles",
+        derechos: (0.18),
+        estadisticas: (0.03),
+        iva: (0.21),
+        impInterno: 0
+        
+    }, 
+    {
+        nombre: "Auriculares",
+        derechos: 0,
+        estadisticas: 0,
+        iva: 0.105,
+        impInterno: 0
+        
+    }, 
+    {
+        nombre: "Calzado",
+        derechos: 0.35,
+        estadisticas: 0.03,
+        iva: 0.21,
+        impInterno: 0
+        
+    }, 
+    {
+        nombre: "Camaras de fotos",
+        derechos: 0,
+        estadisticas: 0,
+        iva: 0.105,
+        impInterno: 0
+        
+    }, 
+    {
+        nombre: "Celulares",
+        derechos: 0.16,
+        estadisticas: 0,
+        iva: 0.21,
+        impInterno: 0.17
+        
+    }, 
+    {
+        nombre: "Consolas Videojuegos",
+        derechos: 0.20,
+        estadisticas: 0.03,
+        iva: 0.21,
+        impInterno: 0
+        
+    }, 
+    {
+        nombre: "Cristaleria",
+        derechos: 0.18,
+        estadisticas: 0.03,
+        iva: 0.21,
+        impInterno: 0
+        
+    }, 
+    {
+        nombre: "Discos Solidos",
+        derechos: 0,
+        estadisticas: 0,
+        iva: 0.105,
+        impInterno: 0
+        
+    }, 
+    {
+        nombre: "Drones",
+        derechos: 0.20,
+        estadisticas: 0.03,
+        iva: 0.21,
+        impInterno: 0
+        
+    }, 
+    {
+        nombre: "Herramientas",
+        derechos: 0.18,
+        estadisticas: 0.03,
+        iva: 0.21,
+        impInterno: 0
+        
+    }, 
+    {
+        nombre: "Juguetes",
+        derechos: 0.35,
+        estadisticas: 0.03,
+        iva: 0.21,
+        impInterno: 0
+        
+    }, 
+    {
+        nombre: "Luces Led",
+        derechos: 0.18,
+        estadisticas: 0.03,
+        iva: 0.21,
+        impInterno: 0
+        
+    }, 
+    {
+        nombre: "Mochilas",
+        derechos: 0.35,
+        estadisticas: 0.03,
+        iva: 0.21,
+        impInterno: 0
+        
+    }, 
+    {
+        nombre: "Monitores",
+        derechos: 0.20,
+        estadisticas: 0.03,
+        iva: 0.21,
+        impInterno: 0.17
+        
+    }, 
+    {
+        nombre: "Notebooks",
+        derechos: 0,
+        estadisticas: 0,
+        iva: 0.105,
+        impInterno: 0
+        
+    }, 
+    {
+        nombre: "Parlantes",
+        derechos: 0.20,
+        estadisticas: 0.03,
+        iva: 0.21,
+        impInterno: 0
+        
+    }, 
+    {
+        nombre: "Respuestos Autos",
+        derechos: 0.18,
+        estadisticas: 0.03,
+        iva: 0.21,
+        impInterno: 0
+        
+    }, 
+    {
+        nombre: "Smartwatch",
+        derechos: 0,
+        estadisticas: 0,
+        iva: 0.105,
+        impInterno: 0
+        
+    }, 
+    {
+        nombre: "Tablets",
+        derechos: 0,
+        estadisticas: 0,
+        iva: 0.105,
+        impInterno: 0
+        
+    }, 
+    {
+        nombre: "Termos",
+        derechos: 0.18,
+        estadisticas: 0.03,
+        iva: 0.21,
+        impInterno: 0
+        
+    }
+
+]
+export {categorias};
--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
-
+import {categorias} from './categorias.js'
 // Elementos
-const categoriaElegida = document.getElementById('CategoriaElegida') ;
-const valorFob = document.getElementById('ValorDeLaCompra');
-const pesoDeLaCompra = document.getElementById('PesoDeLaCompra');
-const agregarItem = document.getElementById('BotonAgregar');
-const contenedorCarrito = document.getElementById('ContenedorCarrito');
+const categoriaElegida = document.getElementById("CategoriaElegida");
+const valorFob = document.getElementById("ValorDeLaCompra");
+const pesoDeLaCompra = document.getElementById("PesoDeLaCompra");
+const agregarItem = document.getElementById("BotonAgregar");
 
 // Variables
 
@@ -12,156 +11,284 @@ const contenedorCarrito = document.getElementById('ContenedorCarrito');
 let impTres = parseFloat(3.8);
 let impUno = parseFloat(1.03);
 
-// Muebles
-let derechosMuebles = 0.17 ; /*  D6 */
-let estadisticasMuebles = 0.03 ; /*  E6 */
-let ivaMuebles = 0.21 ; /* F6 */
-let impIntMuebles = 0.17 ; /* G6 */
-
-// Notebooks
-let derechosNotebook = 0; /* D7 */
-let estadisticasNotebook = 0;  /* E7 */
-let ivaNotebooks = 0.1050; /* F7 */
-let impIntNotebook = 0; /* G7 */
-
-// Celulares
-
-let derechosCelulares = 0.35; /* D8 */
-let estadisticasCelulares = 0.17; /* E8 */
-let ivaCelulares = 0.21; /* F8  */
-let impIntCelulares = 0; /* G8 */
-
 // Declaro variables q luego le asigno valor en los ifs
 let envio;
 let impuestoTotales;
 let totalTraerProducto;
 
-agregarItem.addEventListener('click', () =>{
-
-
-    // ENVIO
-    function peso(){
-    if(pesoDeLaCompra.value <= 5){
-        envio = pesoDeLaCompra.value * 31;
-        // console.log(envio); /* Valor q muestro por carrito */
-
-    } else if (pesoDeLaCompra.value > 5 && pesoDeLaCompra.value < 10){
-        envio = pesoDeLaCompra.value * 26;
-        // console.log(envio); /* Valor q muestro por carrito */
-
+agregarItem.addEventListener("click", () => {
+  // ENVIO
+  function peso() {
+    if (pesoDeLaCompra.value <= 5) {
+      envio = pesoDeLaCompra.value * 31;
+      // console.log(envio); /* Valor q muestro por carrito */
+    } else if (pesoDeLaCompra.value > 5 && pesoDeLaCompra.value < 10) {
+      envio = pesoDeLaCompra.value * 26;
+      // console.log(envio); /* Valor q muestro por carrito */
     } else {
-        envio = pesoDeLaCompra.value * 24;
-        // console.log(envio); /* Valor q muestro por carrito */
-
+      envio = pesoDeLaCompra.value * 24;
+      // console.log(envio); /* Valor q muestro por carrito */
     }
+  }
+  peso();
+
+  // console.log(envio);
+
+  // validacion de categoria
+  if (categoriaElegida.value === "Categorias") {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Por Favor elija una categoria valida.",
+    });
+
+  } else if (valorFob.value === "" || pesoDeLaCompra.value === "" ) {
+    // validacion si valor fob y peso estan completados
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Por favor complete todos los campos.",
+    });
+
+  } else if(pesoDeLaCompra.value > 50 || valorFob.value > 1000){
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Recorda que los topes son  U$S 1000 y hasta 50 Kgs por envío. ",
+    });
+  } else {
+
+      // De lo contrario tiene q realizar esta operacion
+      let categoria = categorias.find((elemento) => elemento.nombre === categoriaElegida.value);
+      console.log(categoria);
+
+      // Funcionalidad de calculadora
+      let baseDerechos =
+            (parseFloat(valorFob.value) +
+              parseFloat(pesoDeLaCompra.value) * impTres) *
+            impUno;
+      console.log('baseDerechos', baseDerechos);
+
+      // Le sumos los impuestos a la base
+      let baseMasImp =
+          baseDerechos * parseFloat(categoria.derechos) +
+          baseDerechos * parseFloat(categoria.estadisticas);
+
+      console.log('baseMasImp', baseMasImp);
+
+      let nuevaBase =
+          baseMasImp +
+          baseDerechos; /* genero una nueva base sumando la basederechos con la basemasimpuestos */
+
+      console.log('nuevabase', nuevaBase);
+
+      // Realizo la suma de nuevaBase mas el iva
+      let baseIva = nuevaBase * parseFloat(categoria.iva);
+      console.log('base Iva', baseIva);
+
+      // Resuelvo suma total de impuestos
+      impuestoTotales = baseIva + baseMasImp;
+      console.log('ImpTotales', impuestoTotales);
+
+      // Total de traer el producto es la suma del envio dependiendo del peso mas el total de impuestos
+      totalTraerProducto = impuestoTotales + envio;
+      console.log('Total', totalTraerProducto);
+
+      // Muestro por consola el valor del producto q ingresaron
+
+      // console.log(valorFob.value);
+      // console.log(envio);
+      // console.log(impuestoTotales);
+      // console.log(totalTraerProducto);
+
+      // Muestro los valores en Carrito
+
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Cotizacion realizada",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+
+      let labelCategoriaElegida = document.getElementById(
+        "labelCategoriaElegida"
+      );
+      let labelEnvio = document.getElementById("labelEnvio");
+      let labelImpuesto = document.getElementById("labelImpuesto");
+      let labelTotal = document.getElementById("labelTotal");
+
+      labelCategoriaElegida.innerText = `${categoriaElegida.value}`;
+      labelEnvio.innerText = `U$S ${new Intl.NumberFormat().format(
+        envio.toFixed(2)
+      )}`;
+      labelImpuesto.innerText = `U$S ${impuestoTotales.toFixed(2)}`;
+      labelTotal.innerText = `U$S ${totalTraerProducto.toFixed(2)}`;
+
+      // Borrar datos de input
+      valorFob.value = "";
+      pesoDeLaCompra.value = "";
+      categoriaElegida.value = "Categorias";
     }
-    peso();
+});
 
-        // console.log(envio);
 
-    // validacion de categoria 
-    if(categoriaElegida.value === 'Categorias' ){
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Por Favor elija una categoria valida.',
-          })
-        // alert('Por Favor elija una categoria valida');
 
-    }else if (valorFob.value === "" || pesoDeLaCompra.value === ""){
-        // validacion si valor fob y peso estan completados  
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Por favor complete todos los campos.',
-          })
-        // alert('Por favor complete todos los campos.')
-    }else{
-        // De lo contrario tiene q realizar esta operacion
-        if(categoriaElegida.value === 'Muebles'){
 
-            // Realizo la primer cuenta baseDerechos
-            let baseDerechos = (parseFloat(valorFob.value) + (parseFloat(pesoDeLaCompra.value) * impTres)) * impUno
-            // console.log(baseDerechos); /* Me dio  */
+// Apuntes cotizador
 
-            // Le sumos los impuestos a la base
-            let baseMasImp = baseDerechos * parseFloat(derechosMuebles) + baseDerechos * parseFloat(estadisticasMuebles); /* 271,71 */
-            let nuevaBase = baseMasImp + baseDerechos; /* genero una nueva base sumando la basederechos con la basemasimpuestos */
-            // console.log(nuevaBase); /* 1630,2 */
+// const contenedorCarrito = document.getElementById("ContenedorCarrito");
+/* // Muebles
+let derechosMuebles = 0.17; 
+let estadisticasMuebles = 0.03; 
+let ivaMuebles = 0.21; 
+let impIntMuebles = 0.17; 
 
-            // Realizo la suma de nuevaBase mas el iva
-            let baseIva = nuevaBase * parseFloat(ivaMuebles)
-            // console.log(baseIva); /* 342,36 */
-            
-            // Resuelvo suma total de impuestos 
-            impuestoTotales = baseIva + baseMasImp;
-            // console.log(impuestoTotales);
+// Notebooks
+let derechosNotebook = 0; 
+let estadisticasNotebook = 0; 
+let ivaNotebooks = 0.105; 
+let impIntNotebook = 0; 
 
-            // Total de traer el producto es la suma del envio dependiendo del peso mas el total de impuestos
-            totalTraerProducto = impuestoTotales + envio;
-            // console.log(totalTraerProducto);
+// Celulares
 
-            // console.log(`Elegiste ${categoriaElegida.value}`);
-        } else if (categoriaElegida.value === 'Notebooks'){
+let derechosCelulares = 0.35; 
+let estadisticasCelulares = 0.17; 
+let ivaCelulares = 0.21; 
+let impIntCelulares = 0;  */
 
-            // Realizo la primer cuenta baseDerechos
-            let baseDerechos = (parseFloat(valorFob.value) + (parseFloat(pesoDeLaCompra.value) * impTres)) * impUno
-            // console.log(baseDerechos); /* Me dio  */
 
-            // Le sumos los impuestos a la base
-            let baseMasImp = baseDerechos * parseFloat(derechosNotebook) + baseDerechos * parseFloat(estadisticasNotebook); /* 271,71 */
-            let nuevaBase = baseMasImp + baseDerechos; /* genero una nueva base sumando la basederechos con la base mas impuestos */
-            // console.log(nuevaBase); /* 1630,2 */
+/* // De lo contrario tiene q realizar esta operacion
+      if (categoriaElegida.value === "Muebles") {
+        // Realizo la primer cuenta baseDerechos
+        let baseDerechos =
+          (parseFloat(valorFob.value) +
+            parseFloat(pesoDeLaCompra.value) * impTres) *
+          impUno;
+        // console.log(baseDerechos); 
 
-            // Realizo la suma de nuevaBase mas el iva
-            let baseIva = nuevaBase * parseFloat(ivaNotebooks)
-            // console.log(baseIva); /* 342,36 */
+        // Le sumos los impuestos a la base
+        let baseMasImp =
+          baseDerechos * parseFloat(derechosMuebles) +
+          baseDerechos * parseFloat(estadisticasMuebles);
+        let nuevaBase =
+          baseMasImp +
+          baseDerechos; 
+        // console.log(nuevaBase); 
 
-            // Resuelvo suma total de impuestos 
-            impuestoTotales = baseIva + baseMasImp;
-            // console.log(impuestoTotales);
+        // Realizo la suma de nuevaBase mas el iva
+        let baseIva = nuevaBase * parseFloat(ivaMuebles);
+        // console.log(baseIva); 
 
-            // Total de traer el producto es la suma del envio dependiendo del peso mas el total de impuestos
-            totalTraerProducto = impuestoTotales + envio;
-            // console.log(totalTraerProducto);
-
-            // console.log(`Elegiste ${categoriaElegida.value}`);
-        } else {
-
-            // Realizo la primer cuenta baseDerechos
-            let baseDerechos = (parseFloat(valorFob.value) + (parseFloat(pesoDeLaCompra.value) * impTres)) * impUno
-            // console.log(baseDerechos); /* Me dio  */
-
-            // Le sumos los impuestos a la base
-            let baseMasImp = baseDerechos * parseFloat(derechosCelulares) + baseDerechos * parseFloat(estadisticasCelulares); /* 271,71 */
-            let nuevaBase = baseMasImp + baseDerechos; /* genero una nueva base sumando la basederechos con la base mas impuestos */
-            // console.log(nuevaBase); /* 1630,2 */
-
-            // Realizo la suma de nuevaBase mas el iva
-            let baseIva = nuevaBase * parseFloat(ivaCelulares)
-            // console.log(baseIva); /* 342,36 */
-
-            // Resuelvo suma total de impuestos 
-            impuestoTotales = baseIva + baseMasImp;
-            console.log(impuestoTotales);
-
-            // Total de traer el producto es la suma del envio dependiendo del peso mas el total de impuestos
-            totalTraerProducto = impuestoTotales + envio;
-            // console.log(totalTraerProducto);
-
-            // console.log(`Elegiste ${categoriaElegida.value}`);
-        }
-
-        // Muestro por consola el valor del producto q ingresaron 
-        // console.log(valorFob.value);
-        // console.log(envio);
+        // Resuelvo suma total de impuestos
+        impuestoTotales = baseIva + baseMasImp;
         // console.log(impuestoTotales);
+
+        // Total de traer el producto es la suma del envio dependiendo del peso mas el total de impuestos
+        totalTraerProducto = impuestoTotales + envio;
         // console.log(totalTraerProducto);
 
-        // Muestro los valores en Carrito
-        // Declaro mi array donde sera guardado el objeto con todos los datos guardados 
+        // console.log(`Elegiste ${categoriaElegida.value}`);
+      } else if (categoriaElegida.value === "Notebooks") {
+        // Realizo la primer cuenta baseDerechos
+        let baseDerechos =
+          (parseFloat(valorFob.value) +
+            parseFloat(pesoDeLaCompra.value) * impTres) *
+          impUno;
+        // console.log(baseDerechos); 
 
-        let cotizacion = [];
+        // Le sumos los impuestos a la base
+        let baseMasImp =
+          baseDerechos * parseFloat(derechosNotebook) +
+          baseDerechos * parseFloat(estadisticasNotebook);
+        let nuevaBase =
+          baseMasImp +
+          baseDerechos; 
+        // console.log(nuevaBase); 
+
+        // Realizo la suma de nuevaBase mas el iva
+        let baseIva = nuevaBase * parseFloat(ivaNotebooks);
+        // console.log(baseIva); 
+
+        // Resuelvo suma total de impuestos
+        impuestoTotales = baseIva + baseMasImp;
+        // console.log(impuestoTotales);
+
+        // Total de traer el producto es la suma del envio dependiendo del peso mas el total de impuestos
+        totalTraerProducto = impuestoTotales + envio;
+        // console.log(totalTraerProducto);
+
+        // console.log(`Elegiste ${categoriaElegida.value}`);
+      } else {
+        // Realizo la primer cuenta baseDerechos
+        let baseDerechos =
+          (parseFloat(valorFob.value) +
+            parseFloat(pesoDeLaCompra.value) * impTres) *
+          impUno;
+        // console.log(baseDerechos); 
+
+        // Le sumos los impuestos a la base
+        let baseMasImp =
+          baseDerechos * parseFloat(derechosCelulares) +
+          baseDerechos * parseFloat(estadisticasCelulares); 
+        let nuevaBase =
+          baseMasImp +
+          baseDerechos; 
+        // console.log(nuevaBase); 
+
+        // Realizo la suma de nuevaBase mas el iva
+        let baseIva = nuevaBase * parseFloat(ivaCelulares);
+        // console.log(baseIva); 
+
+        // Resuelvo suma total de impuestos
+        impuestoTotales = baseIva + baseMasImp;
+        console.log(impuestoTotales);
+
+        // Total de traer el producto es la suma del envio dependiendo del peso mas el total de impuestos
+        totalTraerProducto = impuestoTotales + envio;
+        // console.log(totalTraerProducto); */
+
+        // console.log(`Elegiste ${categoriaElegida.value}`);
+      // }
+
+      // Muestro por consola el valor del producto q ingresaron
+      // console.log(valorFob.value);
+      // console.log(envio);
+      // console.log(impuestoTotales);
+      // console.log(totalTraerProducto);
+
+      // Muestro los valores en Carrito
+      // Swal.fire({
+      //   position: "top-end",
+      //   icon: "success",
+      //   title: "Cotizacion realizada",
+      //   showConfirmButton: false,
+      //   timer: 1500,
+      // });
+
+      // let labelCategoriaElegida = document.getElementById(
+      //   "labelCategoriaElegida"
+      // );
+      // let labelEnvio = document.getElementById("labelEnvio");
+      // let labelImpuesto = document.getElementById("labelImpuesto");
+      // let labelTotal = document.getElementById("labelTotal");
+
+      // labelCategoriaElegida.innerText = `${categoriaElegida.value}`;
+      // labelEnvio.innerText = `U$S ${new Intl.NumberFormat().format(
+      //   envio.toFixed()
+      // )}`;
+      // labelImpuesto.innerText = `U$S ${impuestoTotales.toFixed(2)}`;
+      // labelTotal.innerText = `U$S ${totalTraerProducto.toFixed(2)}`;
+
+      // borrarDatos();
+
+
+
+
+
+// Declaro mi array donde sera guardado el objeto con todos los datos guardados
+
+/*         let cotizacion = [];
 
         const infoCotizacion = {
             valorFob: valorFob.value, 
@@ -173,9 +300,9 @@ agregarItem.addEventListener('click', () =>{
         };
         cotizacion = [...cotizacion, infoCotizacion];
 
-/*         const cartIcon = document.getElementById('cartIcon');
-        cartIcon.innerText = cotizacion.length; */
-        
+        // const cartIcon = document.getElementById('cartIcon');
+        // cartIcon.innerText = cotizacion.length;
+
         console.log(cotizacion);
 
         limpiarHTML()
@@ -212,11 +339,11 @@ agregarItem.addEventListener('click', () =>{
                                         <td>U$D ${totalFinal}</td>
 
                                     </tr>
-        
                                     </tbody>
                                 </table> 
                             `
             contenedorCarrito.append(div);
+
 
             console.log(cotizacion);
 
@@ -229,49 +356,36 @@ agregarItem.addEventListener('click', () =>{
                 })
 
             // alert('Cotizacion realizada')
-            modalContenedor.classList.toggle("carrito-visible");
-        })
-        // Funcion eliminar cotizacion 
+            // modalContenedor.classList.toggle("carrito-visible");
+        }) */
+// Funcion eliminar cotizacion
 
-        const botonEliminar = document.getElementById(`Eliminar`);
+/*         const botonEliminar = document.getElementById(`Eliminar`);
 
         botonEliminar.addEventListener("click", () => {
             console.log("Apretando en el boton eliminar");
             limpiarHTML();
-        });
-
-        borrarDatos ()
-    }
-})
-
-
-
+        }); */
 // Abrir y cerrar carrito
 
-const modalContenedor = document.querySelector("#carrito");
-const abrirCarrito = document.getElementById('botonCart');
-const cerrarCarrito = document.getElementById('cerrar')
+/* const modalContenedor = document.querySelector("#carrito");
+const abrirCarrito = document.getElementById("botonCart");
+const cerrarCarrito = document.getElementById("cerrar"); */
 
-abrirCarrito.addEventListener('click', ()=>{
+/* abrirCarrito.addEventListener('click', ()=>{
     console.log('tocando en boton cart');
     modalContenedor.classList.toggle("carrito-visible");
 })
 
 cerrarCarrito.addEventListener('click', ()=>{
     modalContenedor.classList.remove('carrito-visible')
-})
+}) */
 
 // Averiguar como eliminar el producto sin id
 // Limpiar Html
-function limpiarHTML() {
-    while (contenedorCarrito.firstChild) {
-      contenedorCarrito.removeChild(contenedorCarrito.firstChild);
-    }
-  };
+/* function limpiarHTML() {
+  while (contenedorCarrito.firstChild) {
+    contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+  }
+} */
 
-  // Borrar datos de input
-function borrarDatos (){
-    valorFob.value = '';
-    pesoDeLaCompra.value = '';
-    categoriaElegida.value = 'Categorias';
-}
